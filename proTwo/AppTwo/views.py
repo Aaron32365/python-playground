da7956b7ba855781883d0fd8d6b7c32f890fc33d
Aaron32365 <aaronsphone32365@gmail.com>
2020-11-08 05:28:01 +0000

    added / and /help routes for project two app 1

--- proTwo/AppTwo/views.py
+++ proTwo/AppTwo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
+def  help(request):
+    info = {"template1": "Help Template"}
+    return render(request,"proTwo/index.html",context=info)
+
 def  index(request):
-    return HttpResponse("<em>My second App</em>")
+    info = {"template1": "this is the index page"}
+    return render(request,"proTwo/index.html",context=info)
